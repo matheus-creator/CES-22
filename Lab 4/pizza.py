a09c9b5fab0ff731932d17ddf4ca1498a1628071
Matheus Ramos <msilvaramos84@gmail.com>
2022-06-16 14:54:33 -0300

    Adding documentation

--- Lab 4/pizza.py
+++ Lab 4/pizza.py
@@ -1,45 +1,92 @@
 class PizzaComponent:
+    """
+    Class that represents a pizza
+    """
     def getDescription(self):
+        """
+        Returns the name of the class
+        """
         return self.__class__.__name__
 
+
     def getTotalCost(self):
+        """
+        Returns the cost of the class that represents an ingredient of pizza
+        """
         return self.__class__.cost
 
+
 class Dough(PizzaComponent):
+    """
+    Class that represents a pizza's dough
+    """
     cost = 0.0
 
+
 class Decorator(PizzaComponent):
+    """
+    Abstract class for a pizza decorator
+    """
     def __init__(self, pizzaComponent):
         self.component = pizzaComponent
     
+
     def getTotalCost(self):
+        """
+        Returns the cost of the decorator plus the cost of the decorated
+        """
         return self.component.getTotalCost() + PizzaComponent.getTotalCost(self)
     
+
     def getDescription(self):
+        """
+        Returns the description of the decorator plus the description of the decorated
+        """
         return self.component.getDescription() + '' + PizzaComponent.getDescription(self)
     
+
 class Pepperoni(Decorator):
+    """
+    Decorator for pepperoni ingredient
+    """
     cost = 25.00
     def __init__(self, pizzaComponent):
         Decorator.__init__(self, pizzaComponent)
 
+
 class Cheese(Decorator):
+    """
+    Decorator for cheese ingredient
+    """
     cost = 10.00
     def __init__(self, pizzaComponent):
         Decorator.__init__(self, pizzaComponent)
 
+
 class Catupiry(Decorator):
+    """
+    Decorator for catupiry ingredient
+    """
     cost = 15.00
     def __init__(self, pizzaComponent):
         Decorator.__init__(self, pizzaComponent)
 
+
 class Egg(Decorator):
+    """
+    Decorator for egg ingredient
+    """
     cost = 10.00
     def __init__(self, pizzaComponent):
         Decorator.__init__(self, pizzaComponent)
 
+
+# Creating a pizza with cheese and eggs
+
 cheeseEgg = Egg(Cheese(Dough()))
 print(cheeseEgg.getDescription() + ": $" + str(cheeseEgg.getTotalCost()))
+
+# Creating a pizza with pepperoni and catupiry
 
 pepperoniCatupiry = Catupiry(Pepperoni(Dough()))
 print(pepperoniCatupiry.getDescription() + ": $" + str(pepperoniCatupiry.getTotalCost()))
--- Lab 4/vehicles.py
+++ Lab 4/vehicles.py
@@ -1,47 +1,88 @@
-class Vehicle:  
+class Vehicle:
+    """
+    Class that represents a vehicle
+    """
     def drive(self):
         return "I'm driving a "
+
     
     def addMotor(self, newMotor):
+        """
+        Add a new motor to the vehicle
+        """
         self.motor = newMotor
         print(newMotor.start())
 
+
 class Car(Vehicle):
+    """
+    Class that represents a car
+    """
     def drive(self):
         return Vehicle.drive(self) + "Car"
 
+
     def addMotor(self, newMotor):
         super().addMotor(newMotor)
 
+
 class Truck(Vehicle):
+    """
+    Class that represents a truck
+    """
     def drive(self):
         return Vehicle.drive(self) + "Truck"
 
+
     def addMotor(self, newMotor):
         super().addMotor(newMotor)
 
+
 class Motorcycle(Vehicle):
+    """
+    Class that represents a motorcycle
+    """
     def drive(self):
         return Vehicle.drive(self) + "Motorcycle"
 
+
     def addMotor(self, newMotor):
         super().addMotor(newMotor)
 
+
 class Motor:
+    """
+    Class that represents a motor
+    """
     def start(self):
         return "Starting "
 
+
 class CombustionMotor(Motor):
+    """
+    Class that represents a combustion motor
+    """
     def start(self):
         return Motor.start(self) + "Combustion Motor!"
 
+
 class EletricMotor(Motor):
+    """
+    Class that represents an eletric motor
+    """
     def start(self):
         return Motor.start(self) + "Eletric Motor!"
 
+
 class HybridMotor(Motor):
+    """
+    Class that represents a hybrid motor
+    """
     def start(self):
         return Motor.start(self) + "Eletric Motor!"
+
+
+# Creating a car with eletric motor
 
 car = Car()
 car.addMotor(EletricMotor())
