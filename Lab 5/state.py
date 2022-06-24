d35ec821f122e6af1051146219d4c6690441e3f2
Matheus Ramos <msilvaramos84@gmail.com>
2022-06-24 16:43:13 -0300

    Adding comments to code

--- Lab 5/state.py
+++ Lab 5/state.py
@@ -1,11 +1,18 @@
 from random import randint
 
 def printPrettier():
+    '''
+    Format output.
+    '''
     print()
     print('----------------------------')
     print()
 
+
 class Document:
+    '''
+    Class that represents a document.
+    '''
     def __init__(self, state, statement, author, expiresIn):
         self.statement = statement
         self.author = author
@@ -18,43 +25,85 @@ class Document:
         else:
             self.state = Published(self)
 
+
     def render(self):
+        '''
+        Render document.
+        '''
         self.state.render()
 
+
     def changeState(self, state):
+        '''
+        Change document state.
+        '''
         self.state = state
 
+
     def getDocumentStatement(self):
+        '''
+        Get document statement.
+        '''
         return self.statement
     
+
     def documentAuthor(self):
+        '''
+        Get document author.
+        '''
         return self.author
     
+
     def documentValidUntil(self):
+        '''
+        Get document validation.
+        '''
         return self.expiresIn
 
+
     def publish(self):
+        '''
+        Publish document.
+        '''
         self.state.publish()
 
+
 class State:
+    '''
+    Class that represents a state.
+    '''
     def __init__(self, document):
         self.document = document
         self.author = document.documentAuthor()
 
+
     def render(self):
+        '''
+        Render state.
+        '''
         print(self.document.getDocumentStatement())
 
+
     def publish(self):
+        '''
+        Publish document, which depends on the current state.
+        '''
         pass
 
+
 class Draft(State):
+    '''
+    Class that represents the draft state.
+    '''
     def __init__(self, document):
         super().__init__(document)
+
     
     def render(self):
         super().render()
         print('Documento em construção.')
         printPrettier()
+
     
     def publish(self):
         super().publish()
@@ -63,14 +112,20 @@ class Draft(State):
         else:
             self.document.changeState(Moderation(self.document))
 
+
 class Moderation(State):
+    '''
+    Class that represents the moderation state.
+    '''
     def __init__(self, document):
         super().__init__(document)
+
     
     def render(self):
         super().render()
         print('Documento em revisão.')
         printPrettier()
+
     
     def publish(self):
         super().publish()
@@ -84,13 +139,18 @@ class Moderation(State):
 
 
 class Published(State):
+    '''
+    Class that represents the published state.
+    '''
     def __init__(self, document):
         super().__init__(document)
     
+
     def render(self):
         super().render()
         print('Documento publicado!')
         printPrettier()
+
 
     def publish(self):
         super().publish()
@@ -98,14 +158,18 @@ class Published(State):
             self.document.changeState(Draft(self.document))
             
 
-admin = 'admin'
-member = 'member'
+# Choose the user type
+user = 'member'
+#user = 'admin'
 
-document = Document('draft', '"Meu novo documento."', member, 1)
-#document = Document('draft', '"Meu novo documento."', admin, -1)
+# Change (>=0 for valid and <0, otherwise)
+validUntil = -1
+
+document = Document('draft', '"Meu novo documento."', user, validUntil)
 
 printPrettier()
 
+# Execute state transition
 document.render()
 document.publish()
 document.render()
